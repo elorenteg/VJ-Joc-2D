39baa7dca88d46807d1ef2beb5d45c98f8603c53
Marc <marcvila10@hotmail.com>
2016-03-04 20:35:18 +0100

    Reformat code + Fixed FPS (70)

--- cData.cpp
+++ cData.cpp
@@ -1,7 +1,7 @@
 #include "cData.h"
 
 cData::cData(void) {}
-cData::~cData(void){}
+cData::~cData(void) {}
 
 int cData::GetID(int img)
 {
@@ -10,15 +10,15 @@ int cData::GetID(int img)
 
 void cData::GetSize(int img, int *w, int *h)
 {
-	texture[img].GetSize(w,h);
+	texture[img].GetSize(w, h);
 }
 
 bool cData::LoadImage(int img, char *filename, int type)
 {
 	int res;
 
-	res = texture[img].Load(filename,type);
-	if(!res) return false;
+	res = texture[img].Load(filename, type);
+	if (!res) return false;
 
 	return true;
 }
